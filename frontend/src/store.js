e3497f72ba3df33ea965adbfc6dacf439e8fbc96
Gerson Costa <gcosta.efraim@gmail.com>
2017-10-16 23:49:07 -0400

    Install and create structure redux to application

---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux'
+import reducers from './reducers'
+
+const configStore = () => {
+    return createStore(
+        reducers
+    )
+
+}
+
+export default configStore;
